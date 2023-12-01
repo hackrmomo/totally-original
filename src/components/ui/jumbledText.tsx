82d6ab88db02102df54a1667002f503fd8ab95a5
Mohammad Al-Ahdal <mo@mohammad.dev>
2023-11-30 22:33:51 -0700

    update jumbled-text animation

--- src/components/ui/jumbledText.tsx
+++ src/components/ui/jumbledText.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const jumbledTextCipheredDictionary = ".-/<>?~`!@#$%^&*()_+{}|:\\";
+const jumbledTextCipheredDictionary = ".-/?&*;:^%$#@!";
 const jumbledTextCipheredDictionaryLength = jumbledTextCipheredDictionary.length;
 
 export const JumbledText = ({ text }: { text: string }) => {
   let renderedTextBase = ""
   let resolvedIndices = new Array(text.length).fill(false);
   let threshold = 0.01;
-  const thresholdMultiplier = 1.04 * 66.0 / text.length;
+  const thresholdMultiplier = 1.1;
   const [unresolvedText, setUnresolvedText] = useState<string>("");
   const [resolvedText, setResolvedText] = useState<string>("");
 
@@ -16,33 +16,33 @@ export const JumbledText = ({ text }: { text: string }) => {
     repeatInInterval(() => {
       // start off by initializing the text to start with all -'s one by one
       if (renderedTextBase.length < text.length) {
-        renderedTextBase += "---";
+        renderedTextBase += jumbledTextCipheredDictionary[Math.floor(Math.pow(Math.random(), 7) * jumbledTextCipheredDictionaryLength)] + jumbledTextCipheredDictionary[Math.floor(Math.pow(Math.random(), 7) * jumbledTextCipheredDictionaryLength)]
         renderedTextBase = renderedTextBase.length > text.length ? renderedTextBase.substring(0, text.length) : renderedTextBase;
         setUnresolvedText(renderedTextBase);
-        setResolvedText(renderedTextBase.replace(/-/g, " "));
+        setResolvedText(Array(renderedTextBase.length).fill(" ").join(""));
       }
-      // if the text is fully initialized, start resolving the text
-      else {
-        for (let i = 0; i < text.length; i++) {
-          const shouldResolveCurrentIndex = Math.random() < threshold;
-          if (shouldResolveCurrentIndex && !resolvedIndices[i]) {
-            renderedTextBase = renderedTextBase.substring(0, i) + text[i] + renderedTextBase.substring(i + 1);
-            resolvedIndices[i] = true;
-            setUnresolvedText(resolvedIndices.map((resolved, index) => resolved ? ` ` : renderedTextBase[index]).join(""));
-            setResolvedText(resolvedIndices.map((resolved, index) => resolved ? renderedTextBase[index] : ` `).join(""));
-            threshold *= thresholdMultiplier;
-          } else if (!resolvedIndices[i]) {
-            renderedTextBase = renderedTextBase.substring(0, i) + jumbledTextCipheredDictionary[Math.floor(Math.pow(Math.random(), 7) * jumbledTextCipheredDictionaryLength)] + renderedTextBase.substring(i + 1);
-            setUnresolvedText(resolvedIndices.map((resolved, index) => resolved ? ` ` : renderedTextBase[index]).join(""));
-          }
+      for (let i = 0; i < Math.min(text.length, renderedTextBase.length); i++) {
+        const shouldResolveCurrentIndex = Math.random() < threshold;
+        const shouldReshuffleCurrentIndex = Math.random() < threshold * 3;
+        if (shouldResolveCurrentIndex && !resolvedIndices[i]) {
+          renderedTextBase = renderedTextBase.substring(0, i) + text[i] + renderedTextBase.substring(i + 1);
+          resolvedIndices[i] = true;
+          setUnresolvedText((resolvedIndices.map((resolved, index) => resolved ? ` ` : renderedTextBase[index]).join("")).substring(0, renderedTextBase.length));
+          setResolvedText(resolvedIndices.map((resolved, index) => resolved ? renderedTextBase[index] : ` `).join("").substring(0, renderedTextBase.length));
+        } else if (!resolvedIndices[i] && shouldReshuffleCurrentIndex) {
+          renderedTextBase = renderedTextBase.substring(0, i) + jumbledTextCipheredDictionary[Math.floor(Math.pow(Math.random(), 7) * jumbledTextCipheredDictionaryLength)] + renderedTextBase.substring(i + 1);
+          setUnresolvedText(resolvedIndices.map((resolved, index) => resolved ? ` ` : renderedTextBase[index]).join("").substring(0, renderedTextBase.length));
+          setResolvedText(resolvedIndices.map((resolved, index) => resolved ? renderedTextBase[index] : ` `).join("").substring(0, renderedTextBase.length));
         }
       }
-    }, 50, 10000, () => {
+      threshold *= thresholdMultiplier;
+
+    }, 75, 20000, () => {
       setUnresolvedText("");
       setResolvedText(text);
     });
   }, [text]);
-  
+
   return <StyledSpan unresolvedText={`${unresolvedText}`}>{`${resolvedText}`}</StyledSpan>
 };
 
